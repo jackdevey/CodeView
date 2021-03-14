89fc02e7946f84a4ec84399df4da366906ed8f3a
Jack Devey <jack@bandev.uk>
2021-03-14 16:12:48 +0000

    Add comments

--- CodeView/src/main/java/me/jackdevey/libraries/codeview/CodeView.kt
+++ CodeView/src/main/java/me/jackdevey/libraries/codeview/CodeView.kt
@@ -2,21 +2,37 @@ package me.jackdevey.libraries.codeview
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Color
 import android.util.AttributeSet
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
+/**
+ * The main CodeView element
+ */
 class CodeView : RecyclerView {
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(context, attrs, defStyle)
 
+    /**
+     * The font size of the text
+     */
     var fontSize: Float = 14f
+
+    /**
+     * The code to display on the view
+     */
     var code: String = "No code provided"
+
+    /**
+     * Number each line
+     */
     var numberLines: Boolean = true
 
+    /**
+     * Show all the changes made
+     */
     fun show() {
         val data : MutableList<Line> = mutableListOf()
         for ((i, line) in code.lines().withIndex()) {
@@ -26,6 +42,9 @@ class CodeView : RecyclerView {
         this.layoutManager = LinearLayoutManager(context)
     }
 
+    /**
+     * Draw the element
+     */
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
     }
